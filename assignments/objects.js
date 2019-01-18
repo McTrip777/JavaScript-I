cab3b5b57ca30eeb08a39eb30377c274f48c05fa
McTrip777 <mcjakedude@gmail.com>
2019-01-18 15:07:30 -0800

    objects done

--- assignments/objects.js
+++ assignments/objects.js
@@ -18,27 +18,43 @@ const example = {
 } 
 
 // Write your intern objects here:
+function Intern(id, name, email, gender, funct){
+  this.id = id;
+  this.name = name;
+  this.email = email;
+  this.gender = gender;
+  this.funct = funct;
+}
 
+let intern1 = new Intern(1,"Mitzi", "mmelloy0@psu.edu" ,"F")
+let intern2 = new Intern(2,"Kennan", "kdiben1@tinypic.com" ,"M", function() {return "Hello, my name is Kennan!"})
+let intern3 = new Intern(3,"Keven", "kmummery2@wikimedia.org" ,"M")
+let intern4 = new Intern(4,"Gannie", "gmartinson3@illinois.edu" ,"M")
+let intern5 = new Intern(5,"Antonietta", "adaine5@samsung.com" ,"F", function(x,y){return x*y})
 
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
+console.log(intern1.name);
 
 // Kennan's ID
-
+console.log(intern2.id);
 // Keven's email
-
+console.log(intern3.email);
 // Gannie's name
-
+console.log(intern4.name);
 // Antonietta's Gender
+console.log(intern5.gender);
 
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
-// console.log(kennan.speak());
+//console.log(kennan.speak())
+console.log(intern2.funct());
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
+console.log(intern5.funct(3,4));
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
@@ -49,7 +65,7 @@ const example = {
 // 3. Nest a grandchild object in the child object with properties for name and age.  The name will be Sam and the age will be 30
 // 4. Give each of the objects the ability to speak their names using the this keyword.
 
-const parent = {}
+//const parent = {}
 
 // Log the parent object's name
 
